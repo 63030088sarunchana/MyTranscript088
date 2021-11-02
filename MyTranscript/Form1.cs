8e2dbdf6521c523df4a4e0d10e597bd9e1887dbd
63030088sarunchana <92079547+63030088sarunchana@users.noreply.github.com>
2021-11-03 00:58:49 +0700

    new ui

--- MyTranscript/Form1.cs
+++ MyTranscript/Form1.cs
@@ -16,5 +16,35 @@ namespace MyTranscript
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
