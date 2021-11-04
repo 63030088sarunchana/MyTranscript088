11163133d5f21448d34fb9762bec5927e8765e5d
63030088sarunchana <92079547+63030088sarunchana@users.noreply.github.com>
2021-11-04 22:35:35 +0700

    add code

--- MyTranscript/Form1.cs
+++ MyTranscript/Form1.cs
@@ -17,6 +17,14 @@ namespace MyTranscript
             InitializeComponent();
         }
 
+        class Course
+        {
+            public string CourseID { get; set; }
+            public string CourseName { get; set; }
+            public string CourseCredit { get; set; }
+            public string Grade { get; set; }
+        }
+
         private void label1_Click(object sender, EventArgs e)
         {
 
